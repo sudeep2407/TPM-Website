b0ed189c296db8c82d13d061058eb48aaeb19d88
sudeep2407 <167418494+sudeep2407@users.noreply.github.com>
2024-12-18 21:48:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -435,4 +435,23 @@ document.addEventListener('mouseup', function(e) {
     }
 });
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal, {passive: true});
+
 readRatingData();
+
